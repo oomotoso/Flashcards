d9b71dcd5b803e99a9b5df7877de801ee2238670
Nifemi Omotoso <nifemi.omotosoo@gmail.com>
2021-02-26 15:20:49 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by nifemi omotoso on 2/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
